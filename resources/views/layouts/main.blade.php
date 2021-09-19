5673481e6083b51943e91582b72bec98b2e5e772
AP-Porag <smauhp@gmail.com>
2021-09-20 00:23:08 +0600

    search dropdown in mobile view

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -84,11 +84,8 @@
                 <li><a href="{{route('actors')}}" class="block text-sm px-2 py-4 hover:bg-gray-800 transition duration-300 capitalize">Actor</a></li>
                 <li><a href="{{route('about')}}" class="block text-sm px-2 py-4 hover:bg-gray-800 transition duration-300 capitalize">about</a></li>
             </ul>
-            <div class="relative py-4 px-2">
-                <input type="text" class="bg-gray-800 rounded-full w-64 pl-8 px-4 py-1 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" placeholder="Search">
-                <div class="absolute top-0">
-                    <svg class="fill-current w-4 text-gray-500 mt-6 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
-                </div>
+            <div class="">
+                <livewire:search-dropdown />
             </div>
         </div>
         <script>
